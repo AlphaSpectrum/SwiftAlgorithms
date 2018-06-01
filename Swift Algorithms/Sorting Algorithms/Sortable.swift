c796499015d31423a0cf782bcad424f62dfc9824
Alan Campos <alancampos@me.com>
2018-06-01 12:41:00 -0400

    Added a protocol to conform the sorting algorithms.

---
+++ Swift Algorithms/Sorting Algorithms/Sortable.swift
@@ -0,0 +1,13 @@
+//
+//  Sortable.swift
+//  Swift Algorithms
+//
+//  Created by Alan Campos on 6/1/18.
+//  Copyright © 2018 Alan Campos. All rights reserved.
+//
+
+import Foundation
+
+public protocol Sortable {
+    
+}
